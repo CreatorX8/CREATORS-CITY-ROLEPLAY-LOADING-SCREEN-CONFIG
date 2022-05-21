dad8934f72f9e889b3a207f6850c807068124402
CreatorX8 <90335627+CreatorX8@users.noreply.github.com>
2022-05-21 22:55:25 +0300

    add the config.js file
    Adding the config.js file!

---
+++ config.js
@@ -0,0 +1,38 @@
+var CONFIG = {
+
+    startVolume: 0.1,
+    colorMenuBg: "rgba(150, 150, 150, .5)",
+    logoImgP: "https://cdn.discordapp.com/attachments/976906269266964500/976906359977181204/logo12-1024x1024.png",
+    logoImg: "https://cdn.discordapp.com/attachments/976906269266964500/976906359977181204/logo12-1024x1024.png",
+    videoMedia: "https://www.youtube.com/embed/r5LF2kgu8mg?autoplay=1&%3Fvq=hd720;controls=0;&amp;showinfo=0&loop=1&mute=1&rel=0",
+
+    musicList: [
+        // { name: '', link: '' }
+        { name: 'МУЗИКА', link: 'https://www.youtube.com/watch?v=pxE9jTx9BK4' }
+    ],
+
+    newsContent: [
+        // { img: '', title: '', data: '', text: '' }
+        { img: 'https://cdn.discordapp.com/attachments/976906269266964500/976906359977181204/logo12-1024x1024.png', title: 'НОВОСТ', data: '21.05.2022г.', text: 'ПРОСТО НОВОСТ' }
+    ],
+
+    contactsContent: [
+        // { avatar: '', title: '', status: '', desc: '' }
+        { avatar: 'https://cdn.discordapp.com/attachments/976906269266964500/976906359977181204/logo12-1024x1024.png', title: 'НАШ ЧОВЕК', status: 'БАЧКА ЗА НАС', desc: 'ПИЧ' }
+    ],
+
+    rulesContent: [
+        /* {
+            title: '',
+            text: [
+                { t: '' }
+            ]
+        } */
+        {
+            title: 'ПРАВИЛО',
+            text: [
+                { t: 'ТУКА НЕЩО!' }
+            ]
+        }
+    ]
+};
